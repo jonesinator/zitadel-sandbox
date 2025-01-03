088984d151193515459905b0b0305f5878659a5a
Aaron Jones <aaron@jonesinator.com>
2025-01-02 22:53:48 -0700

    Add scaffolding for frontend.

--- zitadel.tf
+++ zitadel.tf
@@ -38,6 +38,19 @@ resource "zitadel_application_api" "default" {
   auth_method_type = "API_AUTH_METHOD_TYPE_BASIC"
 }
 
+resource "zitadel_application_oidc" "default" {
+  org_id            = zitadel_org.default.id
+  project_id        = zitadel_project.default.id
+  name              = "terraform-test-web"
+  redirect_uris     = ["https://app.example.tld/login-callback"]
+  response_types    = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types       = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
+  app_type          = "OIDC_APP_TYPE_USER_AGENT"
+  auth_method_type  = "OIDC_AUTH_METHOD_TYPE_NONE"
+  version           = "OIDC_VERSION_1_0"
+  access_token_type = "OIDC_TOKEN_TYPE_BEARER"
+}
+
 resource "zitadel_machine_user" "default" {
   org_id      = zitadel_org.default.id
   user_name   = "api.user@example.tld"
@@ -50,6 +63,11 @@ resource "zitadel_personal_access_token" "default" {
   org_id          = zitadel_org.default.id
   user_id         = zitadel_machine_user.default.id
   expiration_date = "2026-01-01T00:40:00Z"
+}
+
+output "web_client_id" {
+  value = zitadel_application_oidc.default.client_id
+  sensitive = true
 }
 
 output "api_client_id" {
